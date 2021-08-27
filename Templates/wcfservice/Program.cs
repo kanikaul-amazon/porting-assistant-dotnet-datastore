e55fac2330a469a7af32c46792242362d52aa0ce
Kanishk Kaul <kanikaul@amazon.com>
2021-08-26 20:16:18 -0700

    Add Template for WCF Service

---
+++ Templates/wcfservice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using CoreWCF.Configuration;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+
+namespace #NAMESPACEPLACEHOLDER#
+{
+	public class Program
+	{
+		public static void Main(string args[])
+		{
+			WebHost.CreateDefaultBuilder(args)
+				 .UseKestrel(options => { })
+				 .UseStartup<Startup>();
+		}
+	}
+}
